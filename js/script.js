238823d94709444e6962cd9b39e369ec27803b8b
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-02-11 07:32:32 +0900

    WORKSもだいたい終わった

--- js/script.js
+++ js/script.js
@@ -60,4 +60,26 @@
       $("#works_item7_content").hide();
       $("#works_item8_content").hide();
     });
+    // 3つ目押したら3つ目表示
+    $("#works_item3").click(function() {
+      $("#works_item1_content").hide();
+      $("#works_item2_content").hide();
+      $("#works_item3_content").show();
+      $("#works_item4_content").hide();
+      $("#works_item5_content").hide();
+      $("#works_item6_content").hide();
+      $("#works_item7_content").hide();
+      $("#works_item8_content").hide();
+    });
+    // 4つ目押した4つ目表示
+    $("#works_item4").click(function() {
+      $("#works_item1_content").hide();
+      $("#works_item2_content").hide();
+      $("#works_item3_content").hide();
+      $("#works_item4_content").show();
+      $("#works_item5_content").hide();
+      $("#works_item6_content").hide();
+      $("#works_item7_content").hide();
+      $("#works_item8_content").hide();
+    });
   });
