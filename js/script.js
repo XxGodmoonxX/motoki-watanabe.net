4bd12f861f45b59616fb11fe9bf5bf0bbc761ca0
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-02-25 03:18:05 +0900

    jQueryでスクロール

--- js/script.js
+++ js/script.js
@@ -95,3 +95,23 @@ $(function() {
     $("#works_item8_content").hide();
   });
 });
+
+// ボタンを押したらスクロールするようにする
+$(function() {
+  //#で始まるリンクをクリックしたら実行
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var speed = 700;
+    //クリックしたaタグのhrefで指定されている値を取得
+    var href = $(this).attr("href");
+    // 移動先を取得
+    var target = $(href == "#" || href == "" ? "html" : href);
+    // 移動先を数値で取得
+    var position = target.offset().top;
+    // スムーススクロール
+    $("body, html").animate(
+      {scrollTop:position}, speed, "swing"
+    );
+    return false;
+  });
+});
