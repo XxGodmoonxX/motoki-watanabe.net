0339b774f1e0fd1ed73bf501caf1fad714adb805
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-06-23 01:15:09 +0900

    add metatag and title

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,7 +3,21 @@ import Link from "next/link";
 import styles from "./layout.module.scss";
 
 function Layout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        ></meta>
+        <title>Motoki Watanabe | Engineer, VJ</title>
+      </Head>
+      <header></header>
+      <div>{children}</div>
+      <footer></footer>
+    </>
+  );
 }
 
 export default Layout;
